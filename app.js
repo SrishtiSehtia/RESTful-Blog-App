a200d229d660e425cf15b8e1659bfb8a1a09026f
Srishti Sehtia <srishti0123@gmail.com>
2017-08-13 04:44:04 +0000

    add create and new routes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require("express"),
     mongoose = require("mongoose"),
     bodyParser = require("body-parser")
     
-mongoose.connect("mongodb://localhost/yelp_camp", {useMongoClient: true});
+mongoose.connect("mongodb://localhost/restful_blog_app", {useMongoClient: true});
 
 // APP CONFIG
 app.set("view engine", "ejs");
@@ -20,19 +20,47 @@ var blogSchema = new mongoose.Schema({
 
 var Blog = mongoose.model("Blog", blogSchema);
 
-// RESTFUL ROUTES
+Blog.create({
+    title: "Test Blog",
+    image: "http://static.tumblr.com/6e76f577f2a8345667bf6408cec8d3a3/butuvak/nxMord2kl/tumblr_static_f14yin0k6kg0k8cok8cw0k0c4.jpg",
+    body: "Universe and thoughts aligned in space and time..."
+})
+
+// RESTFUL ROUTE
 app.get("/", function(req, res){
     res.redirect("/blogs");
 });
 
+// INDEX ROUTE
 app.get("/blogs", function(req, res){
-    Blog.find({}, function(err, blogs){
+   Blog.find({}, function(err, blogs){
+       if(err){
+           console.log("ERROR!");
+       } else {
+          res.render("index", {blogs: blogs}); 
+       }
+   });
+});
+
+// NEW ROUTE
+app.get("/blogs/new", function(req, res){
+    res.render("new");
+});
+
+// CREATE ROUTE
+app.post("/blogs", function(req, res){
+    // create blog
+    console.log(req.body);
+    console.log("===========")
+    console.log(req.body);
+    Blog.create(req.body.blog, function(err, newBlog){
         if(err){
-            console.log(err);
+            res.render("new");
         } else {
-            res.render("index", {blogs: []}); 
+            //then, redirect to the index
+            res.redirect("/blogs");
         }
-    })
+    });
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
